732bd2fad7e68902426c967cc31046dbc474a0f6
Ahmed Kamel <ahmed.kamel.shokr@gmail.com>
2020-12-27 00:09:17 +0200

    restructure the app

---
+++ Entites/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Entites
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HirringDate { get; set; }
+        public int Salary { get; set; }
+    }
+}
