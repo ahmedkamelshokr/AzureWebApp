1b429c600b7e39df31c2ee0f471b73d7329f347f
Ahmed Kamel <ahmed.kamel.shokr@gmail.com>
2020-12-27 00:26:35 +0200

    add solution

---
+++ Manager/EmployeeManager.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using Entites;
+using System;
+
+namespace Manager
+{
+    public class EmployeeManager
+    {
+        private readonly EmployeeRepository repository;
+
+        public EmployeeManager(EmployeeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Employee Get(int empId)
+        {
+            return repository.Get(empId);
+        }
+    }
+}
